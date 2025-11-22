6060113f136ebd1e0efc1e984de53e0eafa8ef6d
Vinicius Alves Fernandes <vini@MacBook-Air-de-Vinicius.local>
2025-11-22 17:43:33 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1C2434',
+        accent: '#00FFB4',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
